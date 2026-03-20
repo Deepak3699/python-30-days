2307ffe728870e1561f2e406f9e40a4aa70d43b1
Deepak <deeppc0101@gmail.com>
2026-03-20 21:26:59 +0530

    Basic Math in Python

---
+++ day01_basics/basic_maths.py
@@ -0,0 +1,9 @@
+# Define two numbers
+a = 10
+b = 5
+
+# Perform arithmetic operations
+print("Sum:", a + b)          # Adds a and b
+print("Difference:", a - b)   # Subtracts b from a
+print("Product:", a * b)      # Multiplies a and b
+print("Quotient:", a / b)     # Divides a by b
--- day01_basics/hello.py
+++ day01_basics/hello.py
@@ -1,2 +1,4 @@
-print(" Hello , python")
+print(" Hello , python") # In this print used to print the text or number write in the ()
 print(" day 1 started")
+print(10+67) # Numbers can directly written in ""
+
