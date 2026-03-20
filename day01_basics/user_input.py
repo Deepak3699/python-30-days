3f315a05b9db22ee81281106821c36ec2f371303
Deepak <deeppc0101@gmail.com>
2026-03-20 21:35:15 +0530

    input file define

---
+++ day01_basics/user_input.py
@@ -0,0 +1,11 @@
+# Ask the user to enter their name
+name = input("Enter your name: ")
+
+# Print a welcome message using the input
+print("Welcome,", name)
+
+
+"""
+The input is used to take the user input like if you want to ask user for his/her name than
+we use input to take a input 
+INPUT  take a string """
